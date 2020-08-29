486ec40d7227c6d203d63bfd2dbce53a3b1e69dc
Cody Alvarez <cody_alvarez19@outlook.com>
2020-08-29 18:04:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,6 +14,7 @@ class App < Sinatra::Base
 
   post '/puppy' do
     @puppy = Puppy.new("name", "breed", "age")
+    @puppy = Puppy.new(params[:name], params[:breed], params[:age])
     erb :display_puppy
   end
 
